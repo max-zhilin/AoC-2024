2bc6955fa878727fb696f7ddcc5f7cf926613fc5
Max Zhilin <max_zhilin@mail.ru>
2024-12-03 11:03:58 +0300

    Day 3+

--- src/Day03.kt
+++ src/Day03.kt
@@ -23,16 +23,27 @@ fun main() {
         return sumMuls(firstLine) + nextLines.sumOf { sumMuls(it) }
     }
 
+    fun part2alt(input: String): Int {
+
+        val regex = "(?=don't[(][)]|do[(][)])".toRegex()    // [(] instead of \(
+
+        return input.split(regex)
+            .filterNot { it.startsWith("don't()") }
+            .sumOf { sumMuls(it) }
+    }
+
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readWholeInput("Day03_test")
     check(part1(testInput) == 161)
     val testInput2 = readWholeInput("Day03_test2")
     //println(part2(testInput2))
     check(part2(testInput2) == 48)
+    check(part2alt(testInput2) == 48)
 
     // Read the input from the `src/Day??.txt` file.
     val input = readWholeInput("Day03")
     part1(input).println()
     val input2 = readWholeInput("Day03")
     part2(input2).println()
+    part2alt(input2).println()
 }
