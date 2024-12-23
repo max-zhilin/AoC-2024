69e477fb453036ed4800937923d19810e8a4ef40
Max Zhilin <max_zhilin@mail.ru>
2024-12-23 10:07:51 +0300

    Day 21 incomplete

--- src/Day21.kt
+++ src/Day21.kt
@@ -152,62 +152,60 @@ fun main() {
         }.sum()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>, steps: Int): Long {
+
+        val dkPaths = mapOf(
+            'A' to 'A' to "",
+            'A' to '^' to "<",
+            'A' to '<' to "v<<",
+            'A' to 'v' to "<v",
+            'A' to '>' to "v",
+            '^' to '^' to "",
+            '^' to 'A' to ">",
+            '^' to '<' to "v<",
+            '^' to 'v' to "v",
+            '^' to '>' to "v>",
+            '<' to '<' to "",
+            '<' to '^' to ">^",
+            '<' to 'A' to ">>^",
+            '<' to 'v' to ">",
+            '<' to '>' to ">>",
+            'v' to 'v' to "",
+            'v' to '^' to "^",
+            'v' to 'A' to "^>",
+            'v' to '<' to "<",
+            'v' to '>' to ">",
+            '>' to '>' to "",
+            '>' to '^' to "^<",
+            '>' to 'A' to "^",
+            '>' to '<' to "<<",
+            '>' to 'v' to "<",
+        )
+
+        val paths = List(steps) { mutableMapOf<Pair<Char, Char>, Long>() }
+
+        dkPaths.forEach { entry ->
+            paths[0][entry.key] = (entry.value + "A").length.toLong()
+        }
+
+        for (n in 1..paths.lastIndex) {
+            dkPaths.forEach { entry ->
+                paths[n][entry.key] = ("A" + entry.value + "A").windowed(2) {
+                    paths[n - 1][it.first() to it.last()]!!
+                }.sum()
+                //paths[n]['A' to 'A'] = 0
+            }
+        }
 
         return input.map { line ->
             line.trimStart('0').dropLast(1).toInt() *
                     ("A" + line).windowed(2) {
                         ("A" + nkPaths[it.first() to it.last()]!! + "A").windowed(2) {
-                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                        ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                        ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                        ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                        ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                        ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                            ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                                ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                                    ("A" + dkAimAndPush[it.first() to it.last()]!!.first + "A").windowed(2) {
-                                                                                                                        dkAimAndPush[it.first() to it.last()]!!.second
-                                                                                                                    }.sum()
-                                                                                                                }.sum()
-                                                                                                            }.sum()
-                                                                                                        }.sum()
-                                                                                                    }.sum()
-                                                                                                }.sum()
-                                                                                            }.sum()
-                                                                                        }.sum()
-                                                                                    }.sum()
-                                                                                }.sum()
-                                                                            }.sum()
-                                                                        }.sum()
-                                                                    }.sum()
-                                                                }.sum()
-                                                            }.sum()
-                                                        }.sum()
-                                                    }.sum()
-                                                }.sum()
-                                            }.sum()
-                                        }.sum()
-                                    }.sum()
-                                }.sum()
-                            }.sum()
+                            paths[steps - 1][it.first() to it.last()]!!
                         }.sum()
                     }.sum()
         }.sum()
+
     }
 
     // Read a small test input from the `src/Day??_test.txt` file:
@@ -218,10 +216,11 @@ fun main() {
         println("Wrong test: " + part1(testInput))
         throw e
     }
-    //check(part2(testInput)() == 29)
+    check(part2(testInput, 2).println() == 126384L)
 
     // Read the input from the `src/Day??.txt` file.
     val input = readInput("Day21")
     //part1(input).println()  // 202274 // 203670 was too high
-    part2(input).println()
+    part2(input, 2).println()
+    part2(input, 25).println()  // 1231184498 too low, 288_468_414_653_042 too high (25, A = 1), 286_140_382_749_046 (25, A = 0) too high, 115_148_904_158_880 (24, A = 1) not right
 }
